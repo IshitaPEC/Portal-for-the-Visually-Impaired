31f5e90eed316fe7da2a7d06a04df64049c74842
IshitaPEC <45597458+IshitaPEC@users.noreply.github.com>
2020-05-30 22:52:00 +0530

    Update views.py

--- mychat_2/login/views.py
+++ mychat_2/login/views.py
@@ -39,74 +39,72 @@ def webcam(request):
 
 def create_dataset(request):
     #print request.POST
+    if request.method=='POST':
+        #Corresponding to this user id, we will have saved the login details of the user
+        userId = request.POST.get('userId')
+        #print (cv2.__version__)
+        # Detect face
+        #Creating a cascade image classifier
+        faceDetect = cv2.CascadeClassifier(BASE_DIR+'/ml/haarcascade_frontalface_default.xml')
+        #capture images from the webcam and process and detect the face
+        # takes video capture id, for webcam most of the time its 0.
+        cam = cv2.VideoCapture(0)
 
-    #Corresponding to this user id, we will have saved the login details of the user
-    #userId = request.POST.get('userId')
+        # Our identifier
+        # We will put the id here and we will store the id with a face, so that later we can identify whose face it is
+        id = userId
+        # Our dataset naming counter
+        sampleNum = 0
+        # Capturing the faces one by one and detect the faces and showing it on the window
+        while(True):
+            # Capturing the image
+            #cam.read will return the status variable and the captured colored image
+            #returns bool variable whether we have capture the image properly
+            ret, img = cam.read()
+            #the returned img is a colored image but for the classifier to work we need a greyscale image
+            #to convert
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #To store the faces
+            #This will detect all the images in the current frame, and it will return the coordinates of the faces
+            #Takes in image and some other parameter for accurate result
+            #1.3 -> scale factor- >How much image size is reduced at each scale
+            #5->min_neighbors-> minimum neighbours of a rectangle that must exist to consider that rectangle
+            #min size, max size -> other arguments of detect multi scale
+            faces = faceDetect.detectMultiScale(gray, 1.3, 5)
+            #In above 'faces' variable there can be multiple faces so we have to get each and every face and draw a rectangle around it.
+            for(x,y,w,h) in faces:
+                # Whenever the program captures the face, we will write that is a folder
+                # Before capturing the face, we need to tell the script whose face it is
+                # For that we will need an identifier, here we call it id
+                # So now we captured a face, we need to write it in a file
+                sampleNum = sampleNum+1
+                # Saving the image dataset, but only the face part, cropping the rest
+                cv2.imwrite(BASE_DIR+'/ml/dataset/user.'+str(id)+'.'+str(sampleNum)+'.jpg', gray[y:y+h,x:x+w])
+                # @params the initial point of the rectangle will be x,y and
+                # @params end point will be x+width and y+height
+                # @params along with color of the rectangle
+                # @params thickness of the rectangle
+                #This is the rectangle which is around our face when we run the command-> green rectangle around our face
+                cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
+                # Before continuing to the next loop, I want to give it a little pause
+                # waitKey of 100 millisecond
+                cv2.waitKey(250)
 
-    #print (cv2.__version__)
-    # Detect face
-    #Creating a cascade image classifier
-    faceDetect = cv2.CascadeClassifier(BASE_DIR+'/ml/haarcascade_frontalface_default.xml')
-    #capture images from the webcam and process and detect the face
-    # takes video capture id, for webcam most of the time its 0.
-    cam = cv2.VideoCapture(0)
-
-    # Our identifier
-    # We will put the id here and we will store the id with a face, so that later we can identify whose face it is
-    id = 1
-    # Our dataset naming counter
-    sampleNum = 0
-    # Capturing the faces one by one and detect the faces and showing it on the window
-    while(True):
-        # Capturing the image
-        #cam.read will return the status variable and the captured colored image
-        #returns bool variable whether we have capture the image properly
-        ret, img = cam.read()
-        #the returned img is a colored image but for the classifier to work we need a greyscale image
-        #to convert
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #To store the faces
-        #This will detect all the images in the current frame, and it will return the coordinates of the faces
-        #Takes in image and some other parameter for accurate result
-        #1.3 -> scale factor- >How much image size is reduced at each scale
-        #5->min_neighbors-> minimum neighbours of a rectangle that must exist to consider that rectangle
-        #min size, max size -> other arguments of detect multi scale
-        faces = faceDetect.detectMultiScale(gray, 1.3, 5)
-        #In above 'faces' variable there can be multiple faces so we have to get each and every face and draw a rectangle around it.
-        for(x,y,w,h) in faces:
-            # Whenever the program captures the face, we will write that is a folder
-            # Before capturing the face, we need to tell the script whose face it is
-            # For that we will need an identifier, here we call it id
-            # So now we captured a face, we need to write it in a file
-            sampleNum = sampleNum+1
-            # Saving the image dataset, but only the face part, cropping the rest
-            cv2.imwrite(BASE_DIR+'/ml/dataset/user.'+str(id)+'.'+str(sampleNum)+'.jpg', gray[y:y+h,x:x+w])
-            # @params the initial point of the rectangle will be x,y and
-            # @params end point will be x+width and y+height
-            # @params along with color of the rectangle
-            # @params thickness of the rectangle
-            #This is the rectangle which is around our face when we run the command-> green rectangle around our face
-            cv2.rectangle(img,(x,y),(x+w,y+h), (0,255,0), 2)
-            # Before continuing to the next loop, I want to give it a little pause
-            # waitKey of 100 millisecond
-            cv2.waitKey(250)
-
-        #This is just to shhow the user how s/he is looking or will look
-        #Showing the image in another window
-        #Creates a window with window name "Face" and with the image img
-        cv2.imshow("Face",img)
-        #Before closing it we need to give a wait command, otherwise the open cv wont work
-        # @params with the millisecond of delay 1
-        cv2.waitKey(1)
-        #To get out of the loop
-        #once we have clicked 35 images of the user, break, no more images needed
-        if(sampleNum>35):
-            break
-    #releasing the cam
-    cam.release()
-    # destroying all the windows
-    cv2.destroyAllWindows()
-
+            #This is just to shhow the user how s/he is looking or will look
+            #Showing the image in another window
+            #Creates a window with window name "Face" and with the image img
+            cv2.imshow("Face",img)
+            #Before closing it we need to give a wait command, otherwise the open cv wont work
+            # @params with the millisecond of delay 1
+            cv2.waitKey(1)
+            #To get out of the loop
+            #once we have clicked 35 images of the user, break, no more images needed
+            if(sampleNum>35):
+                break
+        #releasing the cam
+        cam.release()
+        # destroying all the windows
+        cv2.destroyAllWindows()
     return render(request, 'new.html')
 
 
